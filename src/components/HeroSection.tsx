0f5a762d3bd83762f4a950d1c9e901de72bf780d
atulkumarmanjhi <atulkashyapvlog@gmail.com>
2024-06-22 11:37:37 +0530

    fixing in herosection the heading size

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -12,7 +12,7 @@ function HeroSection() {
         titleComponent={
           <>
           
-            <h1 className="text-4xl md:text-[6rem] font-bold mb-4 leading-none">
+            <h1 className="text-xl sm:text-7xl md:text-[6rem] font-bold mb-4 leading-none">
               Hi, I&apos;m Atul Kashyap<br />
               <p className="text-slate-500 text-xl font-normal">I&apos;m passionate Fullstack Developer with an interest in</p>
               <span className="text-4xl font-semibold text-black dark:text-white">
