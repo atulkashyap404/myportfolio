01677f81f6e57c665caff5d8f47d226796710d17
atulkumarmanjhi <atulkashyapvlog@gmail.com>
2024-06-22 16:03:09 +0530

    update

--- src/components/DatabaseSkills.tsx
+++ src/components/DatabaseSkills.tsx
@@ -1,6 +1,5 @@
-"use client"
+"use client";
 import { HoverEffect } from "./ui/card-hover-effect";
-
 
 interface Language {
   id: number;
@@ -11,39 +10,43 @@ interface Language {
 
 function DatabaseSkills() {
   return (
-    <div className="h-[50rem] w-full overflow-hidden dark:bg-black bg-white  dark:bg-grid-small-white/[0.2] bg-grid-small-black/[0.2] ">
-      <p className="flex justify-center items-center text-3xl text-neutral text-bold pt-8 text-black">Databases</p>
-      <div className=" max-w-7xl mx-auto px-4">
-      <HoverEffect items={language} />
+    <div className="flex flex-col justify-center dark:bg-black bg-white  dark:bg-grid-small-white/[0.2] bg-grid-small-black/[0.2] ">
+      <div className="flex flex-col justify-center  max-auto px-4 sm:px-6">
+        <div className="text-center">
+          <p className="flex justify-center items-center text-3xl text-neutral text-bold pt-8 text-black">
+            Databases
+          </p>
+        </div>
+        <div className=" max-w-7xl mx-auto px-4">
+          <HoverEffect items={language} />
+        </div>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
-
-
 
 export const language = [
   {
     id: 1,
     name: "MongoDB",
-    description: "MongoDB is a popular NoSQL database known for its flexibility and scalability.",
-    link: ""
-    
-},
-{
+    description:
+      "MongoDB is a popular NoSQL database known for its flexibility and scalability.",
+    link: "",
+  },
+  {
     id: 2,
     name: "PostgreSQL",
-    description: "PostgreSQL is a powerful open-source relational database management system known for its robustness and extensibility.",
-    link: ""
-},
-{
+    description:
+      "PostgreSQL is a powerful open-source relational database management system known for its robustness and extensibility.",
+    link: "",
+  },
+  {
     id: 3,
     name: "SQL",
-    description: "QL (Structured Query Language) is used to manage and manipulate relational databases.",
-    link: ""
-},
+    description:
+      "QL (Structured Query Language) is used to manage and manipulate relational databases.",
+    link: "",
+  },
+];
 
-
-]
-
-export default DatabaseSkills
+export default DatabaseSkills;
