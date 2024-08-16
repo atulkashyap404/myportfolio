3ce9e8b3f679cf00d5c593127314de798a45e10a
atulkumarmanjhi <atulkashyapvlog@gmail.com>
2024-08-16 10:55:31 +0530

    dark mode added

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,7 +14,7 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  darkMode: "class",
+  darkMode: "selector",
   theme: {
     extend: {
       backgroundImage: {
